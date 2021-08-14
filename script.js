9bc13f69a156ee87289f8774201cd7aa81942a49
sag10 <56155259+sag10@users.noreply.github.com>
2021-08-15 01:32:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
+     if (principal == "" || principal <= 0) {
+    alert("Enter a positive number");
+     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate / 100) * 10;
