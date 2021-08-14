6d7e8afb7f4bc9d56eb73bdf824c432dabd9c0ff
sag10 <56155259+sag10@users.noreply.github.com>
2021-08-14 22:30:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ function compute()
     var interest = principal * years * rate / 100
     var year = new Date().getFullYear()+parseInt(years);
     
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
+    
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    var x = rateval + "%";
-    document.getElementById("rate_val").innerText=x;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+   
+    document.getElementById("rate_val").innerText=rateval;
 }
 
         
