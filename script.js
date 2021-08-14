39b0d1f2d2a7df14d958d12c482abab53afcbb7a
sag10 <56155259+sag10@users.noreply.github.com>
2021-08-15 01:34:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
-     if (principal == "" || principal <= 0) {
-    alert("Enter a positive number");
-     }
+     
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate / 100) * 10;
     var year = new Date().getFullYear()+parseInt(years);
+    if (principal == "" || principal <= 0) {
+    alert("Enter a positive number");
+     }
+    else{
     var message = "If you deposit <p class=\"show\">"+principal+"</p>,\<br\>at an interest rate of <p class=\"show\">"+rate+"%</p>\<br\>You will receive an amount of <p class=\"show\">"+interest+"</p>,\<br\>in the year <p class=\"show\">"+year+"</p>\<br\>";
     document.getElementById("result").innerHTML=message;
+    }
 }
 function updateRate() 
 {
