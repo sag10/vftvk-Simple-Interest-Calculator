d755d39ca4b785bdd537f9743a35813c2c4914f2
sag10 <56155259+sag10@users.noreply.github.com>
2021-08-15 12:27:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function compute()
   
     if (principal == "" || principal <= 0) {
     alert("Enter a positive number");
+       document.getElementById("principal").focus(); 
     return;
    
      }
@@ -13,9 +14,8 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate / 100) * 10;
     var year = new Date().getFullYear()+parseInt(years);
-    if (principal == "" || principal <= 0) {
-    alert("Enter a positive number");
-    document.getElementById("principal").focus(); 
+    
+    
     
    
     var message = "If you deposit <span class=\"show\">"+principal+"</span>,\<br\>at an interest rate of <span class=\"show\">"+rate+"%</span>\<br\>You will receive an amount of <span class=\"show\">"+interest+"</span>,\<br\>in the year <span class=\"show\">"+year+"</span>\<br\>";
